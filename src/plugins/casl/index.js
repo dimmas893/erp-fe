a2c3a585c240b199a39a50664026c2975429bb4f
knalpotpatah78 <febryputra200@gmail.com>
2025-07-09 08:50:48 +0700

    update show menu

--- src/plugins/casl/index.js
+++ src/plugins/casl/index.js
@@ -3,7 +3,11 @@ import { abilitiesPlugin } from '@casl/vue'
 
 export default function (app) {
   const userAbilityRules = useCookie('userAbilityRules')
-  const initialAbility = createMongoAbility(userAbilityRules.value ?? [])
+  const rules = userAbilityRules.value ?? []
+  const fallBack = [
+    { action: 'manage', subject: 'all' },
+  ]
+  const initialAbility = createMongoAbility(rules.length ? rules : fallBack)
 
   app.use(abilitiesPlugin, initialAbility, {
     useGlobalProperties: true,
